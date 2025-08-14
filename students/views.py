a2b42f80525e5b0efa33cc92b1848937aa35afe8
Dovlet Eminov <dovlet.eminov26.02@gmail.com>
2025-08-14 11:19:22 +0500

    Updated API's

--- students/views.py
+++ students/views.py
@@ -6,10 +6,12 @@ from rest_framework import viewsets
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
 
 class ExerciseResultViewSet(viewsets.ModelViewSet):
     queryset = ExerciseResult.objects.all()
     serializer_class = ExerciseResultSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     
